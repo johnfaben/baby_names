d3ba03278733a0ff0ac3f22998e194052894cdcd
johnfaben <jdfaben@gmail.com>
2020-08-27 13:09:23 +0100

    Fixed formatting for floating point numbers in the output

--- male_female_changes.py
+++ male_female_changes.py
@@ -7,14 +7,14 @@ Created on Thu Jul 23 22:42:13 2020
 
 import os 
 import pandas as pd
+data_folder = 'C:/Users/jdfab/Dropbox/baby_names/baby_names/Data'
 
-
-df = pd.DataFrame(columns = ['year','Name','boys','girls'])
-for file in os.listdir('C:/Users/jdfab/Dropbox/baby_names/Data'):
+df = pd.DataFrame(columns = ['Name','boys','girls','year'])
+for file in os.listdir(data_folder):
     if file[-4:] == '.csv':
         year = int(file[-8:-4])
-        boys = pd.read_csv(file,encoding = 'cp1252',skiprows=6,usecols = [1,2])
-        girls = pd.read_csv(file,encoding = 'cp1252',skiprows =6, usecols = [5,6])
+        boys = pd.read_csv(os.path.join(data_folder,file),encoding = 'cp1252',skiprows=6,usecols = [1,2])
+        girls = pd.read_csv(os.path.join(data_folder,file),encoding = 'cp1252',skiprows =6, usecols = [5,6])
         boys = boys.rename(columns = {'Number of babies':'boys'})
         girls = girls.rename(columns = {'Name.1':'Name','Number of babies.1':'girls'})
         babies = boys.merge(girls,how = 'outer')
@@ -36,5 +36,5 @@ data = data.rename(columns = {'boys_x':'boys15-19','girls_x':'girls15-19','boys_
 data = data.rename(columns = {'ratio_x':'ratio_2019','ratio_y':'ratio_1979'})
 data['change'] = data['ratio_2019']-data['ratio_1979']
 
-print(data.sort_values('change')[['boys15-19','girls15-19','boys74-79','girls74-79','change']].head(10).to_csv())
-print(data.sort_values('change',ascending =False)[['boys15-19','girls15-19','boys74-79','girls74-79','change']].head(10).to_csv())
+print(data.sort_values('change')[['boys15-19','girls15-19','boys74-79','girls74-79','change']].head(10).to_csv(float_format='%.2f'))
+print(data.sort_values('change',ascending =False)[['boys15-19','girls15-19','boys74-79','girls74-79','change']].head(10).to_csv(float_format='%.2f'))
